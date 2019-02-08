9637e2ae93817d9c26981dad4b682237b63cf082
TiffanyCao <tcao00@stuy.edu>
2019-02-08 10:53:24 -0500

    fixed add and remove Queen methods and tested them a bit

--- QueenBoard.java
+++ QueenBoard.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class QueenBoard{
 
   private int[][] board;
@@ -19,6 +21,7 @@ public class QueenBoard{
     if(board[r][c] != 0){ //if the square has been marked or already has a queen
       return false;
     }else{
+      board[r][c] = -1;
       makeX(r, c); //a method to mark the places where this queen can attack
       return false;
     }
@@ -28,13 +31,15 @@ public class QueenBoard{
     for(int i = c+1; i < board[r].length; i++){
       board[r][i] = board[r][i] + 1;
     }
-    for(int x = r+1; x < board.length; x++){
-      board[x][c+1] = board[x][c+1] + 1;
-    }
+    int z = c+1;
     for(int y = r+1; y < board.length; y++){
-      for(int z = c+1; z < board[y].length; z++){
         board[y][z] = board[y][z] + 1;
-      }
+        z++;
+    }
+    z = c+1;
+    for(int y = r-1; y >= 0; y--){
+      board[y][z] = board[y][z] + 1;
+      z++;
     }
   }
 
@@ -52,13 +57,44 @@ public class QueenBoard{
     for(int i = c+1; i < board[r].length; i++){
       board[r][i] = board[r][i] - 1;
     }
-    for(int x = r+1; x < board.length; x++){
-      board[x][c+1] = board[x][c+1] - 1;
-    }
+    int z = c+1;
     for(int y = r+1; y < board.length; y++){
-      for(int z = c+1; z < board[y].length; z++){
         board[y][z] = board[y][z] - 1;
+        z++;
+    }
+    z = c+1;
+    for(int y = r-1; y >= 0; y--){
+      board[y][z] = board[y][z] - 1;
+      z++;
+    }
+  }
+
+  public String toString(){
+    String result = "";
+    for(int i = 0; i < board.length; i++){
+      for(int x = 0; x < board[i].length; x++){
+        if(board[i][x] == -1){
+          result += "Q ";
+        }else{
+          result += "" + board[i][x] + " ";
+        }
+        if(x == board[i].length - 1){
+          result += "\n";
+        }
       }
     }
+    return result;
+  }
+
+
+  public static void main(String[] args){
+    QueenBoard board1 = new QueenBoard(5);
+
+    System.out.println("---Testing AddQueen---");
+    board1.addQueen(2, 0);
+    System.out.println(board1);
+    QueenBoard board2 = board1;
+    board2.removeQueen(2, 0);
+    System.out.println(board2);
   }
 }
